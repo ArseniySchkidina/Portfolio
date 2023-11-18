f032969cf4601fe4410d05a7d1f53d66282736fc
Arseniy Schkidina <gorobchyk@bk.ru>
2023-11-18 18:38:03 +0700

    feat: added animation for burger

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const burger = document.querySelector(".burger");
 const menu = document.querySelector(".mobile-menu");
 burger.addEventListener("click", function () {
     menu.classList.toggle("active");
+    burger.classList.toggle("active");
 })
 
 
