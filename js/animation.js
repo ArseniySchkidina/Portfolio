8ab8725d95c4b0393b28dfc89c573866846350ad
Arseniy Schkidina <gorobchyk@bk.ru>
2023-11-20 18:54:30 +0700

    feat:animation for card(started)

---
+++ js/animation.js
@@ -0,0 +1,56 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const tlStart = gsap.timeline({})
+
+tlStart.from('.author',{
+    x:-400,
+    duration:2,
+    opacity:0,
+    ease: "bounce.out(2)",
+    
+})
+
+gsap.from('.card-skill',{
+    stagger:0.3,
+    opacity:0,
+    y:200,
+    scrollTrigger:{
+        markers:true,
+        trigger: '.my-skills',
+        start: 'top 70%',
+        end:'30% 50%',
+        scrub: 2,
+        toggleActions: 'play none none reverse',
+    } })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 const burger = document.querySelector(".burger");
 const menu = document.querySelector(".mobile-menu");
 burger.addEventListener("click", function () {
