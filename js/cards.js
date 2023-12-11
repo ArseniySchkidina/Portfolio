d232d3c651d61a5e9aba03222e74c274d44f1c37
Arseniy Schkidina <gorobchyk@bk.ru>
2023-12-11 18:49:39 +0700

    feat: add style for cards

--- js/cards.js
+++ js/cards.js
@@ -1,17 +1,22 @@
 const buttons = document.querySelectorAll(".context-button");
 const cross = document.querySelectorAll(".close");
+const fade = document.querySelector(".fade-block");
 
 buttons.forEach(function(button) {
     button.addEventListener("click",(event)=>{
         event.target.closest(".card-skill").classList.add("active");
+        fade.classList.add("open");
     })
 });
 
 cross.forEach(function (btn) {
     btn.addEventListener("click",(event)=>{
         event.target.closest(".card-skill").classList.remove("active");
+        fade.classList.remove("open");
     })
 });
+
+
 
 
 
