ac778c95b62150862cc9b4f0e4bb4630b39d2b8a
Arseniy Schkidina <gorobchyk@bk.ru>
2023-12-23 18:36:24 +0700

    feat: arrows for section review

--- js/slider.js
+++ js/slider.js
@@ -2,5 +2,23 @@ $(".slider-review").slick({
     dots: true,
     infinite: true,
     speed: 300,
-    slidesToShow: 1
+    slidesToShow: 1,
+    prevArrow: 
+    `
+    <button class="slider__arrow slider__arrow--left">
+    <svg width="306" height="306" viewBox="0 0 306 306" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M58.65 267.75L175.95 153L58.65 35.7L94.35 0L247.35 153L94.35 306L58.65 267.75Z" fill="#fff"/>
+    </svg>
+    </button>
+    `
+    ,
+    nextArrow: 
+    `
+    <button class="slider__arrow slider__arrow--right">
+    <svg width="306" height="306" viewBox="0 0 306 306" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M58.65 267.75L175.95 153L58.65 35.7L94.35 0L247.35 153L94.35 306L58.65 267.75Z" fill="#fff"/>
+    </svg>
+    </button>
+    `
+    ,
 });
