acda84711df64a139980731e50e3b93518566a25
Arseniy Schkidina <gorobchyk@bk.ru>
2024-03-02 18:44:13 +0700

    feat: created anchor links for mobile-menu

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
-
+const anchorLinks = document.querySelectorAll(".anchor-link")
 const burger = document.querySelector(".burger");
 const menu = document.querySelector(".mobile-menu");
 burger.addEventListener("click", function () {
     menu.classList.toggle("active");
     burger.classList.toggle("active");
 })
+anchorLinks.forEach(element => {
+    element.addEventListener("click", function () {
+        menu.classList.remove("active");
+        burger.classList.remove("active");
+    })
+});
 
 
 
